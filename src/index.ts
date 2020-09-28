68ba04ff7b00f21af11457566f9859866c6e5465
zhesha <savzhe@gmail.com>
2020-09-28 18:11:01 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import {OrgbValue, Pixels, SetPixel} from "core";
+
+export interface Size {
+  width: number;
+  height: number;
+}
+
+export default function createFramedUpscale(from: Size, to: Size) {
+  const ratioWidth = Math.floor(to.width / from.width);
+  const ratioHeight = Math.floor(to.height / from.height);
+  const ratio = ratioWidth < ratioHeight ? ratioWidth : ratioHeight;
+  const offsetX = Math.floor((to.width - from.width * ratio)/ 2);
+  const offsetY = Math.floor((to.height - from.height * ratio)/ 2);
+  return function (pixels: Pixels): SetPixel {
+    function setPixel (x: number, y: number, color: OrgbValue) {
+      const _x = offsetX + (x * ratio);
+      const _y = offsetY + (y * ratio);
+      for(let i = 0; i < ratio; i++) {
+        for(let j = 0; j < ratio; j++) {
+          pixels.setPixel(_x + i, _y + j, color);
+        }
+      }
+    }
+
+    return setPixel;
+  }
+}
---
+++ test/index.test.ts
@@ -0,0 +1,104 @@
+import createFramedUpscale from "../src";
+
+
+test('asdf', function () {
+  const setMock = jest.fn();
+  const getMock = jest.fn();
+
+  const pixels = {
+    setPixel: setMock,
+    getPixel: getMock,
+  };
+
+  const from = {
+    width: 200,
+    height: 200
+  };
+  const to = {
+    width: 200,
+    height: 200
+  };
+  const scale = createFramedUpscale(from, to);
+  const setPixel = scale(pixels);
+
+  setPixel(0, 0, 1);
+  expect(setMock.mock.calls[0]).toEqual([0, 0, 1]);
+});
+
+test('asdf', function () {
+  const setMock = jest.fn();
+  const getMock = jest.fn();
+
+  const pixels = {
+    setPixel: setMock,
+    getPixel: getMock,
+  };
+
+  const from = {
+    width: 200,
+    height: 200
+  };
+  const to = {
+    width: 200,
+    height: 200
+  };
+  const scale = createFramedUpscale(from, to);
+  const setPixel = scale(pixels);
+
+  setPixel(100, 100, 1);
+  expect(setMock.mock.calls[0]).toEqual([100, 100, 1]);
+});
+
+test('asdf', function () {
+  const setMock = jest.fn();
+  const getMock = jest.fn();
+
+  const pixels = {
+    setPixel: setMock,
+    getPixel: getMock,
+  };
+
+  const from = {
+    width: 100,
+    height: 100
+  };
+  const to = {
+    width: 200,
+    height: 200
+  };
+  const scale = createFramedUpscale(from, to);
+  const setPixel = scale(pixels);
+
+  setPixel(0, 0, 1);
+  expect(setMock.mock.calls[0]).toEqual([0, 0, 1]);
+  expect(setMock.mock.calls[1]).toEqual([0, 1, 1]);
+  expect(setMock.mock.calls[2]).toEqual([1, 0, 1]);
+  expect(setMock.mock.calls[3]).toEqual([1, 1, 1]);
+});
+
+test('asdf', function () {
+  const setMock = jest.fn();
+  const getMock = jest.fn();
+
+  const pixels = {
+    setPixel: setMock,
+    getPixel: getMock,
+  };
+
+  const from = {
+    width: 100,
+    height: 100
+  };
+  const to = {
+    width: 200,
+    height: 200
+  };
+  const scale = createFramedUpscale(from, to);
+  const setPixel = scale(pixels);
+
+  setPixel(10, 10, 1);
+  expect(setMock.mock.calls[0]).toEqual([20, 20, 1]);
+  expect(setMock.mock.calls[1]).toEqual([20, 21, 1]);
+  expect(setMock.mock.calls[2]).toEqual([21, 20, 1]);
+  expect(setMock.mock.calls[3]).toEqual([21, 21, 1]);
+});
